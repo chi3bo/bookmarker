cbbba59bcf5b25e6dc3c079f882bc62502235d53
chi3bo <sh3ban55335@gmail.com>
2023-12-23 22:58:52 +0200

    4 commit (v)

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var siteName = document.getElementById("siteName")
 var siteUrl = document.getElementById("siteUrl")
-var nameRegex= /\w{2,}/i ;
+var nameRegex = /\w{2,}/i;
 var urlRegex = /^((https:\/\/){0,1}(www.){0,1}[A-Za-z0-9]{3,}.[A-Z]{2,4})$/i
 //                 once or not            7rof or numbers mn 3 w tale3    lazem .com     
 var indexUpdate = 0
 
 var linksList = []
 
-function getLocalStorage(){
+function getLocalStorage() {
     var backup = JSON.parse(localStorage.getItem("linksMemory"))
     if (backup != null) {
         linksList = backup
@@ -19,34 +19,34 @@ getLocalStorage()
 
 
 
-function addItems(){
+function addItems() {
 
-    if (nameRegex.test(siteName.value) && urlRegex.test(siteUrl.value))  {
+    if (nameRegex.test(siteName.value) && urlRegex.test(siteUrl.value)) {
         if (siteUrl.value.includes("https://")) {
             var item = {
-                name : siteName.value ,
-                link : siteUrl.value
-            } 
+                name: siteName.value,
+                link: siteUrl.value
+            }
         }
-        else{
+        else {
             var item = {
-                name : siteName.value ,
-                link : "https://"+siteUrl.value
-            } 
+                name: siteName.value,
+                link: "https://" + siteUrl.value
+            }
         }
-    
+
         linksList.push(item)
         showdata()
         clearInputs()
-    
-        localStorage.setItem("linksMemory" , JSON.stringify(linksList)  )
+
+        localStorage.setItem("linksMemory", JSON.stringify(linksList))
 
 
     }
-    else{
+    else {
         document.querySelector("#myModal").classList.remove("d-none")
     }
-// -----------------
+    // -----------------
 
 
 
@@ -55,27 +55,28 @@ function addItems(){
 function clearInputs() {
     siteName.value = ''
     siteUrl.value = ''
-    
+    siteName.classList.remove("is-valid")
+    siteUrl.classList.remove("is-valid" )
 }
 
-function deleteItem(indexDelete){
+function deleteItem(indexDelete) {
 
-    linksList.splice(indexDelete , 1)
+    linksList.splice(indexDelete, 1)
 
-    localStorage.setItem("linksMemory" , JSON.stringify(linksList)  )
+    localStorage.setItem("linksMemory", JSON.stringify(linksList))
     showdata()
 }
 
 
-function showdata(){
+function showdata() {
     shasha = ''
 
-    for (i= 0 ;  i < linksList.length ; i++) {
+    for (i = 0; i < linksList.length; i++) {
 
-        shasha += 
-        `
+        shasha +=
+            `
         <tr>
-        <td>${i+1}</td>
+        <td>${i + 1}</td>
         <td>${linksList[i].name}</td>
         <td> <button class="btn btn-success" > <a href="${linksList[i].link}" target="_blank"> Visit </a> </button> </td>
         <td> <button onclick="deleteItem(${i})" class="btn btn-danger" > Delete </button> </td>
@@ -88,45 +89,44 @@ function showdata(){
 }
 
 function makeSureUrel() {
-   if (urlRegex.test(siteUrl.value)) {
-    document.querySelector("#siteUrl").classList.add("is-valid")
-    document.querySelector("#siteUrl").classList.remove("is-invalid")
-    document.querySelector("#urlAlert").classList.add("d-none")
+    if (urlRegex.test(siteUrl.value)) {
+        document.querySelector("#siteUrl").classList.add("is-valid")
+        document.querySelector("#siteUrl").classList.remove("is-invalid")
+        document.querySelector("#urlAlert").classList.add("d-none")
 
 
-   }
-   else {
-    document.querySelector("#siteUrl").classList.add("is-invalid")
-    document.querySelector("#siteUrl").classList.remove("is-valid")
-    document.querySelector("#urlAlert").classList.remove("d-none")
-   }
+    }
+    else {
+        document.querySelector("#siteUrl").classList.add("is-invalid")
+        document.querySelector("#siteUrl").classList.remove("is-valid")
+        document.querySelector("#urlAlert").classList.remove("d-none")
+    }
 }
 
 function makeSureName() {
     if (nameRegex.test(siteName.value)) {
-     document.querySelector("#siteName").classList.add("is-valid")
-     document.querySelector("#siteName").classList.remove("is-invalid")
-     document.querySelector("#nameAlert").classList.add("d-none")
+        document.querySelector("#siteName").classList.add("is-valid")
+        document.querySelector("#siteName").classList.remove("is-invalid")
+        document.querySelector("#nameAlert").classList.add("d-none")
 
- 
+
     }
     else {
-     document.querySelector("#siteName").classList.add("is-invalid")
-     document.querySelector("#siteName").classList.remove("is-valid")
-     document.querySelector("#nameAlert").classList.remove("d-none")
+        document.querySelector("#siteName").classList.add("is-invalid")
+        document.querySelector("#siteName").classList.remove("is-valid")
+        document.querySelector("#nameAlert").classList.remove("d-none")
 
     }
- }
+}
 
 
-document.querySelector("#myModal").addEventListener("click" , getTarget)
-
+document.querySelector("#myModal").addEventListener("click", getTarget)
 function getTarget(e) {
-    var myClick = e.target.id     
-    if (myClick  == "myModal" || myClick  == "xMark" ) {
+    var myClick = e.target.id
+    if (myClick == "myModal" || myClick == "xMark") {
         document.querySelector("#myModal").classList.add("d-none")
     }
-    else{
+    else {
         console.log("stay");
     }
 
